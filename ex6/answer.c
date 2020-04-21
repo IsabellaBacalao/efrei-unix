3a2921b16ef1b9ffadbc531d4dea2a625210b553
isabella bacalao <isabella.bacalao@decathlon.com>
2020-04-21 11:21:40 +0200

    cours 21/04/2020 et exo 6

---
+++ ex6/answer.c
@@ -0,0 +1,16 @@
+#include "unistd.h"
+
+//You have to create a C function that should take a char in parameter and print it
+
+void display_char(char c){
+  //print c
+  write(1, &c, 1);
+}
+
+int main(int ac, char **av){
+  char c = 'a';
+
+  display_char(c);
+
+  return (0);
+}
